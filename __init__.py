e71abbf4e53408ae2f7807b5b0e0b7d9a828dec5
lansdell <lansdell@uw.edu>
2014-12-09 11:52:21 -0800

    Init

---
+++ __init__.py
@@ -0,0 +1,24 @@
+def which(program):
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+def system(cmd):
+	import subprocess
+	p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	out = p.stdout.read()
+	err = p.stderr.read()
+	return (out, err)
