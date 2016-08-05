3fbe1394066476efbeef95ba1a1f31352ba8a433
Ben Lansdell <lansdell@uw.edu>
2016-08-05 13:46:05 -0700

    Add set operations on 2D numpy arrays

--- __init__.py
+++ __init__.py
@@ -22,3 +22,37 @@ def system(cmd):
     out = p.stdout.read()
     err = p.stderr.read()
     return (out, err)
+
+#Numpy array set operations on 2D array
+def intersect2d(A, B):
+    #A = np.array([[1,4],[2,5],[3,6]])
+    #B = np.array([[1,4],[3,6],[7,8]])
+    import numpy as np
+    nrows, ncols = A.shape
+    dtype={'names':['f{}'.format(i) for i in range(ncols)],
+           'formats':ncols * [A.dtype]}
+    C = np.intersect1d(A.view(dtype), B.view(dtype))
+    C = C.view(A.dtype).reshape(-1, ncols)
+    return C 
+
+def union2d(A, B):
+    #A = np.array([[1,4],[2,5],[3,6]])
+    #B = np.array([[1,4],[3,6],[7,8]])
+    import numpy as np
+    nrows, ncols = A.shape
+    dtype={'names':['f{}'.format(i) for i in range(ncols)],
+           'formats':ncols * [A.dtype]}
+    C = np.union1d(A.view(dtype), B.view(dtype))
+    C = C.view(A.dtype).reshape(-1, ncols)
+    return C 
+
+def setdiff2d(A, B):
+    #A = np.array([[1,4],[2,5],[3,6]])
+    #B = np.array([[1,4],[3,6],[7,8]])
+    import numpy as np
+    nrows, ncols = A.shape
+    dtype={'names':['f{}'.format(i) for i in range(ncols)],
+           'formats':ncols * [A.dtype]}
+    C = np.setdiff1d(A.view(dtype), B.view(dtype))
+    C = C.view(A.dtype).reshape(-1, ncols)
+    return C 
